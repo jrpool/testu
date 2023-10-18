867d8d89254a99a953bdb9743ec98e3bb25d3026
Jonathan Robert Pool <jonathan.pool@cvshealth.com>
2023-10-17 21:02:35 -0400

    Debugged

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ const requestHandler = async (request, response) => {
       response.end(script);
     }
     // Otherwise, if it is for the application icon:
-    else if (requestURL === '/favicon.ico') {
+    else if (requestURL === '/testu/favicon.ico') {
       // Serve nothing.
       response.end('');
     }
